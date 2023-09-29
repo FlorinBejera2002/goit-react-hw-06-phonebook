c5a06c0e771b7f8c6965d54a239b6e60cb3bb89b
FlorinBejera2002 <florinpetru0306@gmail.com>
2023-09-29 11:28:41 +0300

    commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import Container from '@mui/material/Container';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import { Title, ListTitle } from './App.styled';
+
+const App = () => {
+  return (
+    <Container className="container" maxWidth="sm" sx={{ mt: 2 }}>
+      <Title>Phonebook</Title>
+      <ContactForm />
+      <ListTitle>Contacts</ListTitle>
+      <Filter />
+      <ContactList />
+    </Container>
+  );
+};
+
+export default App;
